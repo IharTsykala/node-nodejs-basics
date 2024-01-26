342119244a0e0ac6abfed1e5a7f67b47840215d2
IharTsykala <ihatsykala@kitrum.com>
2024-01-27 02:25:57 +0600

    feat: add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { readFile } from 'node:fs/promises';
+
+import { fileURLToPath } from 'url';
+import path from 'path';
+import crypto from "crypto";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+	const currentPath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+	
+	const buffer = await readFile(currentPath);
+	
+	const hash = crypto.createHash('SHA256');
+	
+	const fileHash = hash.update(buffer).digest('hex');
+	console.log(fileHash);
 };
 
 await calculateHash();
