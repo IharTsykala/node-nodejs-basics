c9889fd8d1bc13ca2b8858ad5533d0cb0932940e
IharTsykala <ihatsykala@kitrum.com>
2024-01-27 02:44:50 +0600

    Revert "feat: add cli functions"
    This reverts commit 9a4a4ce25000055d8c73b4bc5fd1bdc8602fb7ee.

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,13 +1,5 @@
-const { argv } = process;
-
 const parseArgs = () => {
-	const args = argv.slice(2);
-	
-	args.forEach((arg, index) => {
-		if (arg.startsWith('--')) {
-			console.log(`${arg.slice(2)} is ${args[index + 1]}`);
-		}
-	});
+    // Write your code here 
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,15 +1,5 @@
-const PREFIXES = {
-	RSS_:'RSS_'
-}
-
-const ENV = process.env
-
 const parseEnv = () => {
-	for (let key in ENV) {
-		if (key.startsWith(PREFIXES.RSS_)) {
-			console.log(`${key}=${ENV[key]}`);
-		}
-	}
+    // Write your code here 
 };
 
 parseEnv();
