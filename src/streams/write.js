9fb3d247a29b7d167f8c94da6636e192502ca67c
IharTsykala <ihatsykala@kitrum.com>
2024-01-28 18:31:48 +0600

    feat: add streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,33 @@
+import fs from 'fs'
+
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+//constants
+import {FILES, FOLDERS} from "./constants.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+	const readPath = path.join(__dirname, FOLDERS.FILES, FILES.WRITE_FILE)
+	
+	const writeStream = fs.createWriteStream(readPath, { flags: 'a' });
+	
+	process.stdin.pipe(writeStream);
+	
+	writeStream.write('Hello world!');
+	writeStream.write('it is test');
+
+	writeStream.on('finish', () => {
+		console.log('Data was written in fail');
+	});
+
+	writeStream.on('error', (err) => {
+		console.error('Error by reading:', err);
+	});
+	
+	writeStream.end()
 };
 
 await write();
