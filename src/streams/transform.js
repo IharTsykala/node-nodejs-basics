61a0b37c168545008d2930568c9303fdecd341ad
IharTsykala <ihatsykala@kitrum.com>
2024-01-28 20:30:21 +0600

    feat: add cp function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 
 //constants
-import {FILES, FOLDERS} from "./constants.js";
+import { FILES, FOLDERS } from "./constants.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
